6d0074c41f780c15fa110c22c7c72b3d88e33e82
ziweizheng <vickyzheng1314@gmail.com>
2022-05-12 18:03:41 -0400

    Update variables.tf
    add aws access credentials to allow tfc operations run and apply to aws

--- variables.tf
+++ variables.tf
@@ -13,3 +13,13 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS_ACCESS_KEY_ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS_SECRET_ACCESS_KEY"
+  type        = string
+}
+
